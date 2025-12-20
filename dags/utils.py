92058014b84684d245f4aab6d1365c95fb7e54fa
manny-hub <ayomideadeyeyee1@gmail.com>
2025-12-20 17:38:24 +0100

    corrected the file structure

--- dags/utils.py
+++ dags/utils.py
@@ -1,3 +1,14 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+if parent_dir not in sys.path:
+    sys.path.insert(0, parent_dir)
+    print(f"Added {parent_dir} to sys.path")
+
+
 from extract_staging import ingest_weather_data
 from stage_transform import transform
 from load import load
